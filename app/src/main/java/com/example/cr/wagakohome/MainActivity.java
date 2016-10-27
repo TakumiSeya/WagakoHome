34290e97aa7184e712f46571f178a36f311850ac
Takumi Seya <b1014246@fun.ac.jp>
2016-10-27 21:10:58 +0900

    Initial commit

---
+++ app/src/main/java/com/example/cr/wagakohome/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.cr.wagakohome;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TabHost;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        initTabs();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void initTabs() {
+        try {
+            TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+            tabHost.setup();
+            TabHost.TabSpec spec;
+
+            // Tab1
+            spec = tabHost.newTabSpec("Tab1")
+                    .setIndicator("Home")
+                    .setContent(R.id.linearLayout);
+            tabHost.addTab(spec);
+
+            // Tab2
+            spec = tabHost.newTabSpec("Tab2")
+                    .setIndicator("Camera")
+                    .setContent(R.id.linearLayout2);
+            tabHost.addTab(spec);
+
+            // Tab3
+            spec = tabHost.newTabSpec("Tab3")
+                    .setIndicator("Forms")
+                    .setContent(R.id.linearLayout3);
+            tabHost.addTab(spec);
+
+            tabHost.setCurrentTab(0);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+}
