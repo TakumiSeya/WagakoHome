57337d6e518d7812ddf7762ce2442cad5de9cbfd
Takumi Seya <b1014246@fun.ac.jp>
2016-11-02 15:47:48 +0900

    before joint

--- app/src/main/java/com/example/cr/wagakohome/HomeMenu.java
+++ app/src/main/java/com/example/cr/wagakohome/HomeMenu.java
@@ -1,7 +1,7 @@
 package com.example.cr.wagakohome;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class HomeMenu extends AppCompatActivity {
 
@@ -9,5 +9,6 @@ public class HomeMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_menu);
+
     }
 }
